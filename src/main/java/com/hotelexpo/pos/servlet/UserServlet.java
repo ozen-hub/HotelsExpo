2ec590deb19a4d7d7faa83ad8b28478c3595d8c6
ozen-hub <info.ozenhub@gmail.com>
2022-11-14 21:53:55 +0530

    18

--- src/main/java/com/hotelexpo/pos/servlet/UserServlet.java
+++ src/main/java/com/hotelexpo/pos/servlet/UserServlet.java
@@ -1,4 +1,18 @@
 package com.hotelexpo.pos.servlet;
 
-public class UserServlet {
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet("/user")
+public class UserServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        PrintWriter writer = resp.getWriter();
+        writer.println("<h1>Hello User</h1>");
+    }
 }
